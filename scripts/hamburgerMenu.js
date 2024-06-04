8bfd923d0f85f39353c9954f0252c2032a939237
galliem <gar22094@byui.edu>
2024-06-04 14:30:32 -0600

    small changes

--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -1,5 +1,5 @@
 let menu_button = document.getElementById("menu-button")
-let navlist = document.querySelector("nav ul")
+let navlist = document.querySelector(`.navigation`)
 menu_button.addEventListener('click', () =>{
     menu_button.classList.toggle("open")
     navlist.classList.toggle("open")
