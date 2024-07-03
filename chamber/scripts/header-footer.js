4f900ea8ca8b782d8473eec0bc27f83b041d11f2
galliem <gar22094@byui.edu>
2024-07-03 14:19:57 -0600

    color changes

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -19,19 +19,19 @@ darkbutton.addEventListener("click", () =>{
     if (darkbutton.classList.contains("dark")){
         document.documentElement.style.setProperty(`--text-color`, `white`);
         document.documentElement.style.setProperty(`--background-color`, `#0D0221`);
-        document.documentElement.style.setProperty(`--accent-color`, `#D36135`);
-        document.documentElement.style.setProperty(`--header-color`, `#E6AA668`);
+        document.documentElement.style.setProperty(`--accent-color`, `#4D937B`);
+        document.documentElement.style.setProperty(`--header-color`, `#D36135`);
         document.documentElement.style.setProperty(`--nav-color`, `#717EC3`);
-        document.documentElement.style.setProperty(`--inv-text-color`, `#0D0221`);
+        document.documentElement.style.setProperty(`--inv-text-color`, `black`);
         document.documentElement.style.setProperty(`--card-color`, `#0D0221`);
     }
     else{
-        document.documentElement.style.setProperty(`--text-color`, `#0D0221`);
-        document.documentElement.style.setProperty(`--background-color`, `#ECE4B7`);
-        document.documentElement.style.setProperty(`--accent-color`, `#D36135`);
+        document.documentElement.style.setProperty(`--text-color`, `black`);
+        document.documentElement.style.setProperty(`--background-color`, `#F6F3DF`);
+        document.documentElement.style.setProperty(`--accent-color`, `#4D937B`);
         document.documentElement.style.setProperty(`--header-color`, `#E6AA68`);
         document.documentElement.style.setProperty(`--nav-color`, `#717EC3`);
         document.documentElement.style.setProperty(`--inv-text-color`, `white`);
-        document.documentElement.style.setProperty(`--card-color`, `#0D0221`);
+        document.documentElement.style.setProperty(`--card-color`, `#1E044E`);
     }
 })
