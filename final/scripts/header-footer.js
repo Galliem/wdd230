29a9c95c0bc38f27d72442ae84dd0f265ddc2497
galliem <gar22094@byui.edu>
2024-07-11 15:23:06 -0600

    added images and favicon and meta tags

--- final/scripts/header-footer.js
+++ final/scripts/header-footer.js
@@ -12,3 +12,31 @@ menubutton.addEventListener("click", () =>{
     menubutton.classList.toggle("open")
     nav.classList.toggle("open")
 })
+
+//View Products button
+function openJoinPage(){
+    window.open("products.html")
+}
+
+document.querySelector(".hero button").addEventListener("click", openJoinPage)
+
+//Import products info  (enters data from JSON file)
+
+const url = `./data/products.json`
+const cards = document.querySelector('#data')
+
+const displayProphets = (cards) => {
+  cards.forEach((card) => {
+    let newsection = document.createElement("section");
+    newsection.innerHTML = `
+                <h2>${card.name}</h2>
+                <p>${card.number}<p>
+                <p>${card.season}<p>
+                <p>${card.type}<p>
+                <img src="${card.imageURL}" alt="${card.name} image" loading="lazy" height="400">`
+        cards.append(newsection)
+  });
+
+}
+
+getProphetData()
